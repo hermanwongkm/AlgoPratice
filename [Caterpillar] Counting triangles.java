9534bf18874dc5086e1f6e65adce8f662931745b
Herman Wong <herman_wong_km@outlook.com>
2020-01-31 23:09:15 +0800

    Update [Caterpillar] Counting triangles.java

--- [Caterpillar] Counting triangles.java
+++ [Caterpillar] Counting triangles.java
@@ -57,3 +57,27 @@ class Solution {
         return count;
     }
 }
+
+// you can also use imports, for example:
+import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        int count =0;
+        Arrays.sort(A);
+        //If the array is sorted, P+R > Q and Q+R >P will automatically be true 
+        for(int i = 0; i < A.length - 2; i++){
+            for(int j = i +1; j < A.length - 1; j++){
+                int k = j + 1;
+                while(k < A.length && A[i] + A[j] > A[k]){
+                    k++;
+                }
+                count = count + (k-j)-1;
+            }
+        }
+        return count;
+    }
+}
