5963235cdd21648b4ae8c20e5e19a7f61a25ab96
Herman Wong <herman_wong_km@outlook.com>
2020-02-11 19:45:40 +0800

    Create [Array] max product contigious.java

---
+++ [Array] max product contigious.java
@@ -0,0 +1,33 @@
+/**
+Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+Example 1:
+
+Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+
+**/
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = nums[0];
+        int min = nums[0];
+        int result = nums[0];
+        
+        for(int i = 1; i < nums.length; i++){
+            int temp = max;
+            //If it is all positive, the max will continue to grow
+            //If it was previously a min (negative) and this is also a negative it will become positive
+            //Its possible that the previous numbers are 0 so you need to max with the current element.
+            max = Math.max(Math.max(nums[i] * max, nums[i] * min),nums[i]); 
+            //But you also want to keep track of the min since if its a negative number and the next might make it positive
+            min = Math.min(Math.min(nums[i] * temp, nums[i] * min), nums[i]);
+            
+            if(max > result){
+                result = max;
+            }
+        }
+        return result;
+    }
+}
