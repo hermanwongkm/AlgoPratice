f7eadda4f47078309fdc3c66e8f00052b4155f33
Herman Wong <herman_wong_km@outlook.com>
2020-04-19 23:35:59 +0800

    Update [Array] max product contigious.java

--- [Array] max product contigious.java
+++ [Array] max product contigious.java
@@ -20,6 +20,8 @@ class Solution {
             //If it is all positive, the max will continue to grow
             //If it was previously a min (negative) and this is also a negative it will become positive
             //Its possible that the previous numbers are 0 so you need to max with the current element.
+            //Even if the current value is 0, you are still multiplying. And Thus it will become 0 no matter what. Thus thats not why you math.max with nums[i]
+            //You do that because, if the previous value is 0, you want to take the current index as value 
             max = Math.max(Math.max(nums[i] * max, nums[i] * min),nums[i]); 
             //But you also want to keep track of the min since if its a negative number and the next might make it positive
             min = Math.min(Math.min(nums[i] * temp, nums[i] * min), nums[i]);
