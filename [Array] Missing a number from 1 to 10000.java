dada83889086a46588ac12d8321da638b00d4f4c
Herman Wong <herman_wong_km@outlook.com>
2020-04-20 13:37:19 +0800

    Update [Array] Missing a number from 1 to 10000.java

--- [Array] Missing a number from 1 to 10000.java
+++ [Array] Missing a number from 1 to 10000.java
@@ -30,8 +30,6 @@ Write an efficient algorithm for the following assumptions:
 // you can also use imports, for example:
 import java.util.*;
 
-// you can write to stdout for debugging purposes, e.g.
-// System.out.println("this is a debug message");
 
 class Solution {
     public int solution(int[] A) {
