8642ed7aa3a12a289d72b837e2e123b2db1c0450
Herman Wong <herman_wong_km@outlook.com>
2020-05-12 15:06:17 +0800

    Create [Array] array rotation.js

---
+++ [Array] array rotation.js
@@ -0,0 +1,46 @@
+/**
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Follow up:
+
+    Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+    Could you do it in-place with O(1) extra space?
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    const rotations = k % nums.length;
+    let count = 0;
+    for(let i = 0; i <= k; i++){
+        if(count === nums.length){
+            break;
+        }
+        let flag = true;
+        let prevValue = nums[i];
+        let currIndex = i;
+        while(flag){
+            currIndex = (currIndex + rotations) % nums.length;
+            let temp = nums[currIndex];
+            nums[currIndex] = prevValue;
+            prevValue = temp;
+            count++;
+            if(currIndex === i){
+                flag = false;
+            }
+        }
+    }
+};
