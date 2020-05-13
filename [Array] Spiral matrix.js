11d1a09f7c387af3127b84e782da6b5bf5a90d19
Herman Wong <herman_wong_km@outlook.com>
2020-05-13 21:10:19 +0800

    Update and rename [array] Spiral matrix.js to [Array] Spiral matrix.js

--- [Array] Spiral matrix.js
+++ [Array] Spiral matrix.js
@@ -15,22 +15,22 @@ var spiralOrder = function(matrix) {
     
     while(r1 <= r2 && c1 <= c2){
         //Now i want to move across the columns
-        for(let i = c1; i <= c2; i++){
+        for(let i = c1; i <= c2; i++){ //<= because i want to include the whole row
             ans.push(matrix[r1][i]);
         }
         //Now i want to move down
-        for(let j = r1+1; j <= r2; j++){
+        for(let j = r1+1; j <= r2; j++){ //+1 because the previous one took up the first value in this column
             ans.push(matrix[j][c2]); //c2 because last column
         }
         if(r1 < r2 && c1 < c2){//this is for the case of [[3],[2]]
             
 
         //Now i want to move left
-        for(let i = c2-1; i > c1; i--){
+        for(let i = c2-1; i > c1; i--){ //The previous one took up the value of this row and it is > but not >= because it does not include the last value
             ans.push(matrix[r2][i]);
         }
         //Now i want to move up
-        for(let j = r2; j > r1; j--){
+        for(let j = r2; j > r1; j--){ //Same as above, where it does not include the last value.
             ans.push(matrix[j][c1]);
         }
         }
