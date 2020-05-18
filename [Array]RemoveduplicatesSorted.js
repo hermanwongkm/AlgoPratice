3d6f4a3dfb99d55d4fc79c8551d112dfd4934eba
Herman Wong <herman_wong_km@outlook.com>
2020-05-19 01:49:50 +0800

    Update and rename [Array] Remove duplicates.js to [Array]RemoveduplicatesSorted.js

--- [Array]RemoveduplicatesSorted.js
+++ [Array]RemoveduplicatesSorted.js
@@ -8,17 +8,15 @@ Example 1:
 Given nums = [1,1,2],
 
 Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
-
-It doesn't matter what you leave beyond the returned length.
  */
+
 var removeDuplicates = function(nums) {
     let indexToOverwrite = 1;
     for(let i = 1; i < nums.length; i++){
         if(nums[i] !== nums[i-1]){
-            console.log("came here")
             nums[indexToOverwrite] = nums[i];
             indexToOverwrite++;
-        }
+        } //If it is the same, you just move forward as per normal
     }
     return indexToOverwrite
 };
