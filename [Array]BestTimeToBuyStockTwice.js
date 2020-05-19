65a4efc7e1020ccf80f71913116523e9c651eeb7
Herman Wong <herman_wong_km@outlook.com>
2020-05-19 13:12:50 +0800

    Update and rename [Array] Best time to buy stock twice.js to [Array]BestTimeToBuyStockTwice.js

--- [Array]BestTimeToBuyStockTwice.js
+++ [Array]BestTimeToBuyStockTwice.js
@@ -7,7 +7,9 @@ Note: You may not engage in multiple transactions at the same time (i.e., you mu
 
 **/
 
-
+//Record the maximum profit possible in an array in the first forward pass.
+//Next, you do a backward pass and compute the max profit backwards.
+//Then, you can add the current profit + the max profit from the previous days possible. 
 var maxProfit = function(prices) {
     let firstArray = []
     let minPrice = Number.MAX_VALUE
@@ -20,7 +22,6 @@ var maxProfit = function(prices) {
         maxProfitSoFar = Math.max(maxProfitSoFar, prices[i] - minPrice)
         firstArray.push(maxProfitSoFar)
     }
-    console.log(firstArray)
     //Now, the second loop backwards calculate the same thing but backwards.
     //Then, you add with the previous array but -1 from the index, as you cannot buy and sell on the same day.
     let maxPrice = Number.MIN_VALUE
