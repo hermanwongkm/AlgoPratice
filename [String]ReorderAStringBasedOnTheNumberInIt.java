e2bafb9a1a1b1090c10e8676221207af520dc0e6
Herman Wong <herman_wong_km@outlook.com>
2020-05-28 20:52:20 +0800

    Update and rename Reorder a string based on the number in it.java to [String]ReorderAStringBasedOnTheNumberInIt.java

--- [String]ReorderAStringBasedOnTheNumberInIt.java
+++ [String]ReorderAStringBasedOnTheNumberInIt.java
@@ -2,36 +2,6 @@
 //e.g. thi1s i1s a4 sentenc2e.
 //Note that instead of using a hashmap, I can insert it directly into the array itself. 
 
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-/*
- * Complete the 'reorderSentence' function below.
- *
- * The function is expected to return a STRING.
- * The function accepts STRING input as parameter.
- */
 
 function reorderSentence(input) {
     //Assume it started with 0
@@ -39,7 +9,7 @@ function reorderSentence(input) {
     //no dup
     // always by spaces
     
-    /**
+    
      *  String[] strArrary = input.splt(" ");
      *  HashMap<Integer, String> map = new HashMap();
      *  String[] answer = new String[strArray.length];
@@ -63,28 +33,5 @@ function reorderSentence(input) {
      *      answer[mapElement.getKey() - 1] = mapElement.getValue();    
      * }
      * return answer.join(" ");
-     */
-}
-
-/**
- * HERE
- * 1. loop through the list for each string
- *     1.1 loop through the string to find the position
- *          1.2 Store number as key and value as the string in the hashmap
- * 
- * since i know the size of list, i can create a array of size 5
- * loop thorugh the hashmap, using the key i will insert it into the array
- * []
- */
-
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const input = readLine();
-
-    const result = reorderSentence(input);
-
-    ws.write(result + '\n');
-
-    ws.end();
+    
 }
