474e57db994a06b0c86c1d831c96e8fec789f405
Herman Wong <herman_wong_km@outlook.com>
2020-05-31 21:50:23 +0800

    Update and rename [Array] Winter Summer to [Array]WinterSummer.java

--- [Array]WinterSummer.java
+++ [Array]WinterSummer.java
@@ -11,7 +11,7 @@ class Solution {
     public int solution(int[] T) {
     //Winter always start at index 0
     //Max temperature in winter so far
-    int maxWinter = T[0];
+    int maxWinter = T[0]; //When it says maxWinter, it means it is the lowest value so far. Dont be mistaken.
     int maxTemp = T[0];
     int ans = 1; 
     for(int i = 0; i < T.length; i++){
