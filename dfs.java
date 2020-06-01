8d06a80585740041bd140ebc9b585c7793320b4c
Herman Wong <herman_wong_km@outlook.com>
2020-06-01 17:57:07 +0800

    Update and rename dfs to dfs.java

--- dfs.java
+++ dfs.java
@@ -23,7 +23,7 @@ public class Solution {
             } 
             else if(line.equals("Q2")){
                 int ans = countConnectedGraph(id,adjacencyList);
-                System.out.println(ans -1);
+                System.out.println(ans);
             }
             else{
                 String[] numArray = line.split("-");
@@ -44,6 +44,7 @@ public class Solution {
                         int index1 = mapping.get(firstNum);
                         int index2 = mapping.get(secondNum);
                         adjacencyList.get(index1).add(index2);
+                        adjacencyList.get(index2).add(index1);
                     }
                     
                     map.get(firstNum).add(secondNum);
@@ -57,7 +58,8 @@ public class Solution {
                 //Add to graph
                     int index1 = mapping.get(firstNum);
                     int index2 = mapping.get(secondNum);
-                    adjacencyList.get(index1).add(index2);
+                        adjacencyList.get(index1).add(index2);
+                        adjacencyList.get(index2).add(index1);
                     
                 }
                 
@@ -65,6 +67,7 @@ public class Solution {
                         if(!map.get(firstNum).contains(secondNum)){
                             int index1 = mapping.get(firstNum);
                             int index2 = mapping.get(secondNum);
+                            adjacencyList.get(index1).add(index2);
                             adjacencyList.get(index2).add(index1);
                     }
                     
@@ -79,7 +82,9 @@ public class Solution {
                                     //Add to graph
                     int index1 = mapping.get(firstNum);
                     int index2 = mapping.get(secondNum);
-                    adjacencyList.get(index2).add(index1);
+                        adjacencyList.get(index1).add(index2);
+                        adjacencyList.get(index2).add(index1);
+
                 }
            } 
     
@@ -127,3 +132,82 @@ public class Solution {
     } 
 
 }
+
+/**
+A pandemic has broken out and efforts to perform contact tracing is ongoing.
+A mobile contact tracing app has been developed, and you are tasked with analyzing the logs.
+2 people may meet more than once, but this is still considered as having 1 contact.
+As entries are added to the log from top to bottom, there will be 2 types of queries made about the data so far, Q1 and Q2.
+
+    Q1: For the person(s) who contacted the most people, how many people did he/she contact?
+    Q2: How many contact clusters are there, at the point of query?
+
+Number of contact clusters refers to the number of disconnected groups of people. (See image)
+
+image
+
+There will be 20 test cases, and the first 10 will only contain Q1 and not Q2.
+Q1 is easier to solve, and it is recommended that you solve this first.
+
+Q2 is intentionally much harder than Q1, do not worry if you cannot solve it. :)
+
+Input Format
+
+    Each line of the log indicates when 2 persons (indicated by phone number M) are in contact with each other.
+    The 2 phone numbers on a line will not be the same.
+    The 2 phone numbers on a line will be separated by a single dash.
+    There will be a maximum of N lines in the log.
+    If a query appears before any contacts are logged, print 0
+
+Constraints
+
+1 <= M <= 1,000,000
+1 <= N <= 500,000
+
+Output Format
+
+An integer every time there is a query.
+
+Sample Input 0
+
+1-2
+Q1
+2-3
+1-4
+5-1
+1-5
+1-5
+Q1
+
+Sample Output 0
+
+1
+3
+
+Explanation 0
+
+On the first query, both person's 1 and 2 have contacted each other, and both have a contact count of 1. Therefore the highest number of contacts a person has made is 1, and we print 1.
+
+On the second query, person 1 has contacted 3 other people (2, 4, 5), and has the highest contact count. Therefore we print 3.
+
+Sample Input 1
+
+1-2
+Q1
+2-3
+1-4
+5-1
+Q1
+Q2
+6-7
+Q2
+
+Sample Output 1
+
+1
+3
+1
+2
+
+
+**/
